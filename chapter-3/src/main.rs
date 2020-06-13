8173189a87932e1e5cd8e92555a59cfab8c0cc94
Jason N <me@sibicle.com>
2020-06-12 22:57:27 -0600

    tuples

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+  tup();
+}
+
+fn shadow() {
     let x = 5;
 
     let x = x + 1;
@@ -6,4 +10,12 @@ fn main() {
     let x = x * 2;
 
     println!("The value of x is: {}", x);
+}
+
+fn tup() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
 }
