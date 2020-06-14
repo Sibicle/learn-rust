e1a11ec351a0ff4e6c4f188cdd5e40015675c1ef
Jason N <me@sibicle.com>
2020-06-13 19:30:24 -0600

    ocd

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -1,8 +1,22 @@
 fn main() {
   shadow();
+
   tup();
+
   tuptwo();
+
   // err();
+
+  basic();
+
+  params(43);
+
+  str_params("hello".to_string(), 43);
+
+  array(4, 2);
+  // array(8); // out of bounds
+
+  state();
 }
 
 fn shadow() {
@@ -29,11 +43,47 @@ fn tuptwo() {
     println!("The value of tuple(x, y, z) is: ({}, {}, {})", tup.0, tup.1, tup.2);
 }
 
-fn err() {
+fn _err() {
     let a = [1, 2, 3, 4, 5];
     let index = 10;
 
     let element = a[index];
 
     println!("The value of element is: {}", element);
+}
+
+fn basic() {
+    println!("Another function.");
+}
+
+fn params(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn str_params(x: String, y: i32) {
+    println!("{}, {}", x, y);
+}
+
+fn array(n: usize, m: usize) {
+    let a: [usize; 7] = [1, 7, 2, 6, 18, 5, 70];
+
+    println!("The value at position {} is: {}", n, a[n]);
+
+    let b = [4, 5, 7, 8];
+
+    println!("The value {} is at position {} of {}", b[m], m, b.len());
+}
+
+fn state() {
+    println!("{}", express(5));
+}
+
+fn express(num: usize) -> String {
+    let mut numm = num;
+
+    for _ in 1..num {
+        numm = numm + num;
+    }
+
+    format!("{} meows", numm)
 }
