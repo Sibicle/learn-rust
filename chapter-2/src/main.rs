89e9b8d8647a50e6699cfac3c82c0cd4ff610e6e
Jason N <me@sibicle.com>
2020-06-14 14:58:11 -0600

    improvements to guessing range

--- chapter-2/src/main.rs
+++ chapter-2/src/main.rs
@@ -1,16 +1,23 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use std::io::Write;
 
 fn main() {
   println!("welcome to the game.");
+  println!("you must find the number");
 
   let secret = rand::thread_rng().gen_range(1, 101);
 
   loop {
-    println!("you must choose:");
 
     let mut guess = String::new();
+
+    print!("you say ");
+
+    io::stdout()
+      .flush()
+      .expect("das bad");
 
     io::stdin()
       .read_line(&mut guess)
@@ -24,15 +31,42 @@ fn main() {
       }
     };
 
-    println!("you say {}", guess);
-
     match guess.cmp(&secret) {
-      Ordering::Less => println!("I say try larger"),
-      Ordering::Greater => println!("I say try smaller"),
+      Ordering::Less => larger(secret, guess),
+      Ordering::Greater => smaller(secret, guess),
       Ordering::Equal => {
           println!("I say yes.");
           break;
       }
     }
   }
+}
+
+fn larger(secret: u32, guess: u32) {
+  distance(secret, guess, true);
+}
+
+fn smaller(secret: u32, guess: u32) {
+  distance(secret, guess, false);
+}
+
+fn distance(secret: u32, guess: u32, dir: bool) {
+  let size = if dir { "larger" } else { "smaller" };
+
+  let delta = if dir {
+    (secret - guess) / 10
+  } else {
+    (guess - secret) / 10
+  };
+
+  let amount = match delta {
+    6..=10 => String::from(format!("much, much, much {}", size)),
+    4..=5  => String::from(format!("a lot {}", size)),
+    2..=3  => String::from(format!("{}", size)),
+    1      => String::from(format!("a little bit {}", size)),
+    0      => String::from(format!("a teensy bit {}", size)),
+    _      => String::from("something who knows")
+  };
+
+  println!("I say try {}", amount);
 }
