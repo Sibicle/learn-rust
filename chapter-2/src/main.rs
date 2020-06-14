2ee65cba2001932ef397cec2372093a0d7b06902
Jason N <me@sibicle.com>
2020-06-14 00:25:48 -0600

    guessing game

--- chapter-2/src/main.rs
+++ chapter-2/src/main.rs
@@ -1,15 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+  println!("welcome to the game.");
 
-    println!("Please input your guess.");
+  let secret = rand::thread_rng().gen_range(1, 101);
+
+  loop {
+    println!("you must choose:");
 
     let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+      .read_line(&mut guess)
+      .expect("das bad");
 
-    println!("You guessed: {}", guess);
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("try again");
+        continue;
+      }
+    };
+
+    println!("you say {}", guess);
+
+    match guess.cmp(&secret) {
+      Ordering::Less => println!("I say try larger"),
+      Ordering::Greater => println!("I say try smaller"),
+      Ordering::Equal => {
+          println!("I say yes.");
+          break;
+      }
+    }
+  }
 }
